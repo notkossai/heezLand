1907f345bfe126ee113eff2c855537115a467b6c
notkossai <medjadikossai@outlook.com>
2025-11-19 00:53:42 +0100

    WIP (HomePage, IV): Initial version Home Page
    what: Responsive design, Stucture, Elements, Minimum
    styling.
    
    why: /
    
         #Version States
             #IV: Initial Version
             #OWV: On Work Version
             #PFV: Probably Final Version
             #FV: Final Version
         #Work Type
             #TODO
             #DONE
             #BUG
             #DEBUG
             #FIX
             #TEST
             #PERF
             #STYLE
             #PRETTY: Clean Code
             #REVERT: Revert To Previous Commit
             #FEAT: New Feature
             #REFACTOR: Code Chnage(not debug, not feature)
             #WIP: Work In Progress
    Refs: #ISSUE

--- src/features/main/home/components/HeroSection.jsx
+++ src/features/main/home/components/HeroSection.jsx
@@ -11,9 +11,21 @@ import HeroLogoMdSvg from "@/assets/svg/brand/circle_msct_heez.svg?react"
 export default function HeroSection() {
     return (
         <section className="hero-section">
+
+            <div className="tagline">
+                <p id="typingText">Learn, Play, Recycle - The eco-education 
+                    platform for kids & families.
+                </p>
+            </div>
+
             <div className="logo-wrap">
-                <HeroLogoSvg className="logo Xl" />
+                <HeroLogoSvg className="logo xl" />
                 <HeroLogoMdSvg className="logo md" />
+            </div>
+
+            <div className="quick-link">
+                <button className="toKids">Go to kids</button>
+                <button className="toParents">Go to parents</button>
             </div>
             <div className="grass-wrap">
                  <GrassSvg />
